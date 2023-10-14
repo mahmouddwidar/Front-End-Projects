d42a018498650c14c51907736af8f1dc552929f1
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2023-10-14 16:13:22 +0300

    Initial commit: uploading weather app files

---
+++ Weather App/js/main.js
@@ -0,0 +1,28 @@
+const apiKey = 'd44063330bac39082c4f20ec79a3c771';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon =document.querySelector('.weather-icon');
+const weatherInfo = document.querySelector('.weather');
+const errorInfo = document.querySelector('.error');
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    if (response.status == 404) {
+        errorInfo.style.display = 'block';
+        weatherInfo.style.display = 'none';
+    } else {
+        document.querySelector('.city').innerHTML = data.name;
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+        document.querySelector('.humditiy').innerHTML = data.main.humidity + '%';
+        document.querySelector('.wind').innerHTML = data.wind.speed + 'Km/h';
+        weatherIcon.src = `./imgs/icons/${data.weather[0].main}.png` // Change weather icon on city current state
+        weatherInfo.style.display = 'block'
+    }
+
+}
+
+searchBtn.addEventListener('click', ()=> checkWeather(searchBox.value))
